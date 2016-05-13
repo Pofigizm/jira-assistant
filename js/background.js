91978af4b5dfda71e084b08d1f8c0b204c6c18d4
Denis Iogansen <pofigizm@gmail.com>
2016-05-13 13:26:57 +0300

    code style

--- js/background.js
+++ js/background.js
@@ -1,34 +1,35 @@
 // background script of the plugin
 
-;(function () {
-    'use strict';
+;
+(function() {
+  'use strict';
 
-    /**
-     * Copy text into the clipboard using Google Chrome extension API
-     * @param {String} text Text to copy
-     */
-    function copyToClipboard( text ){
-        var copyDiv = document.createElement('div'),
-            status;
+  /**
+   * Copy text into the clipboard using Google Chrome extension API
+   * @param {String} text Text to copy
+   */
+  function copyToClipboard(text) {
+    var copyDiv = document.createElement('div'),
+      status;
 
-        copyDiv.contentEditable = true;
-        document.body.appendChild(copyDiv);
-        copyDiv.innerHTML = text;
-        copyDiv.unselectable = 'off';
-        copyDiv.focus();
-        document.execCommand('selectAll');
-        status = document.execCommand('copy', false, null);
-        document.body.removeChild(copyDiv);
+    copyDiv.contentEditable = true;
+    document.body.appendChild(copyDiv);
+    copyDiv.innerHTML = text;
+    copyDiv.unselectable = 'off';
+    copyDiv.focus();
+    document.execCommand('selectAll');
+    status = document.execCommand('copy', false, null);
+    document.body.removeChild(copyDiv);
 
-        return status;
+    return status;
+  }
+
+  chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.copy) {
+      sendResponse(
+        copyToClipboard(request.copy)
+      );
     }
-
-    chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
-        if ( request.copy ) {
-            sendResponse(
-                copyToClipboard( request.copy )
-            );
-        }
-    });
+  });
 
 }());
--- js/contentScript.js
+++ js/contentScript.js
@@ -2,79 +2,82 @@
 /* eslint-disable one-var */
 /* eslint-disable vars-on-top */
 
-;(function () {
-    'use strict';
-    var title, button;
+;
+(function() {
+  'use strict';
+  var title, button;
 
-    function getTitle() {
-      var taskNumber = document.querySelector('#key-val');
-      var taskTitle = document.querySelector('#summary-val');
+  function getTitle() {
+    var taskNumber = document.querySelector('#key-val');
+    var taskTitle = document.querySelector('#summary-val');
 
-      if ( !taskNumber || !taskTitle ) {
-          return false;
+    if (!taskNumber || !taskTitle) {
+      return false;
+    }
+
+    return taskNumber.innerText + ': ' + taskTitle.innerText;
+  }
+
+  /**
+   * @todo Research, how we could normally insert our group into the page
+   */
+  function createCopyButton() {
+    var newTitle = getTitle();
+    if (!newTitle) return;
+
+    if (newTitle !== title) {
+      title = newTitle;
+      button = null;
+    }
+
+    if (button) return;
+
+
+    var copyButton = document.createElement('a'),
+      toolbarItem = document.createElement('li'),
+      copiedItem = document.createElement('span'),
+      toolbarGroup = document.createElement('ul'),
+      lastToolbarGroup = document.getElementById('opsbar-opsbar-transitions');
+
+    copyButton.href = 'javascript:void(0);';
+    copyButton.title = 'Copy task title into the clipboard';
+    copyButton.innerHTML = '<span class="icon copy-icon"></span><span class="trigger-text"> Copy Task Title</span>';
+    copyButton.className = 'toolbar-trigger';
+
+    toolbarItem.className = 'toolbar-item toolbar-item-copy';
+    toolbarItem.appendChild(copyButton);
+
+    copiedItem.innerText = 'copied';
+    copiedItem.className = 'toolbar-item-copy-message';
+
+    toolbarItem.appendChild(copiedItem);
+
+    toolbarGroup.className = 'toolbar-group';
+    toolbarGroup.appendChild(toolbarItem);
+
+    copyButton.addEventListener('click', function() {
+      copyToClipboard(function() {
+        copiedItem.classList.add('toolbar-item-copy-message__visible');
+        setTimeout(function() {
+          copiedItem.classList.remove('toolbar-item-copy-message__visible');
+        }, 450);
+      });
+    });
+
+    // insert created toolbar after last one
+    button = lastToolbarGroup.parentNode
+      .insertBefore(toolbarGroup, lastToolbarGroup.nextSibling);
+  }
+
+  function copyToClipboard(callback) {
+    chrome.extension.sendMessage({
+      copy: title
+    }, function(response) {
+      if (response) {
+        callback();
       }
+    });
+  }
 
-      return taskNumber.innerText + ': ' + taskTitle.innerText;
-    }
-
-    /**
-     * @todo Research, how we could normally insert our group into the page
-     */
-    function createCopyButton() {
-        var newTitle = getTitle();
-        if (!newTitle) return;
-
-        if (newTitle !== title) {
-          title = newTitle;
-          button = null;
-        }
-
-        if (button) return;
-
-        
-        var copyButton = document.createElement('a'),
-            toolbarItem = document.createElement('li'),
-            copiedItem = document.createElement('span'),
-            toolbarGroup = document.createElement('ul'),
-            lastToolbarGroup = document.getElementById('opsbar-opsbar-transitions');
-
-        copyButton.href = 'javascript:void(0);';
-        copyButton.title = 'Copy task title into the clipboard';
-        copyButton.innerHTML = '<span class="icon copy-icon"></span><span class="trigger-text"> Copy Task Title</span>';
-        copyButton.className = 'toolbar-trigger';
-
-        toolbarItem.className = 'toolbar-item toolbar-item-copy';
-        toolbarItem.appendChild( copyButton );
-
-        copiedItem.innerText = 'copied';
-        copiedItem.className = 'toolbar-item-copy-message';
-
-        toolbarItem.appendChild( copiedItem );
-
-        toolbarGroup.className = 'toolbar-group';
-        toolbarGroup.appendChild( toolbarItem );
-
-        copyButton.addEventListener('click', function () {
-            copyToClipboard(function () {
-                copiedItem.classList.add('toolbar-item-copy-message__visible');
-                setTimeout(function () {
-                    copiedItem.classList.remove('toolbar-item-copy-message__visible');
-                }, 450);
-            });
-        });
-
-        // insert created toolbar after last one
-        button = lastToolbarGroup.parentNode
-            .insertBefore(toolbarGroup, lastToolbarGroup.nextSibling);
-    }
-
-    function copyToClipboard(callback) {
-        chrome.extension.sendMessage({ copy: title }, function (response) {
-            if ( response ) {
-                callback();
-            }
-        });
-    }
-
-    setInterval(createCopyButton, 1000);
+  setInterval(createCopyButton, 1000);
 }());
